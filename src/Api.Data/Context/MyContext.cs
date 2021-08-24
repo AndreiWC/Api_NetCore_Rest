11eb64db588c859f182b8c6e3c3761b24576c33e
AndreiWC <37909054+AndreiWC@users.noreply.github.com>
2021-08-24 10:34:07 -0300

    FOI

---
+++ src/Api.Data/Context/MyContext.cs
@@ -0,0 +1,22 @@
+using Api.Data.Mapping;
+using Api.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Api.Data.Context
+{
+    public class MyContext : DbContext
+    {
+        public DbSet<UserEntity> User { get; set; }
+
+        public MyContext(DbContextOptions<MyContext> options) : base(options) { }
+        // CONTEXTOFACTORY IRÁ PROVER A CONEXÃO PARA TEMPO DE DESGNER E O USERMAP CONFIGURA A TABELA NO BANCO DE DADOS
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<UserEntity>(new UserMap().Configure);
+        }
+
+    }
+
+
+}
---
+++ src/Api.Data/Mapping/UserMap.cs
@@ -0,0 +1,26 @@
+using Microsoft.EntityFrameworkCore;
+using Api.Domain.Entities;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Api.Data.Mapping
+{
+    // MAPEIA AS ENTITIES
+    public class UserMap : IEntityTypeConfiguration<UserEntity>
+    {
+        // EJETA A ENTITY USERENTITY PARA O MEETODO CONFIGURE
+        public void Configure(EntityTypeBuilder<UserEntity> builder)
+        {
+            //COM A ENTITY RECEBIDA PASSA OS COMANDOS PARA SER CONFIGURADA A TABELA
+           // CRIA O NOME DA TABELA
+            builder.ToTable("User");
+            //CRIA O INDEX DA TABELA
+            builder.HasKey(u => u.Id);
+            //CRIA UM INDEX DA TABELA - CAMPO UNICO
+            builder.HasIndex(u => u.Email).IsUnique();
+            //CRIA O CAMPO NAME COM A PROPRIEDADE REQUERIDA(não pode ser null) E TAMANHO MAXIMO DE 60 CARCTERES
+            builder.Property(u => u.Name).IsRequired().HasMaxLength(60);
+            // CRIA O CAMPO EMAIL COM TAMANHO MAXIMO DE 100 CARACTERES
+            builder.Property(u => u.Email).HasMaxLength(100);
+        }
+    }
+}
