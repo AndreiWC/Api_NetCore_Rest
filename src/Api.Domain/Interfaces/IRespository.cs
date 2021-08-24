891b44ff333a95da0666b7b479ac53aa69e2d012
AndreiWC <37909054+AndreiWC@users.noreply.github.com>
2021-08-24 14:51:33 -0300

    Criado IRespository
    Criado IRespository

---
+++ src/Api.Domain/Interfaces/IRespository.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Api.Domain.Entities;
+
+namespace Api.Domain.Interfaces
+{
+    /*
+    IRespository É UMA INTERFACE QUE OBRIGA A IMPLMENTAÇÃO DOS METODOS A ONDE FOR HERDADA A CLASSE
+    ELA É RESPONSAVEL POR CRIAR OS METODOS CRUD DAS ENTITIES
+    ELA RECEBE UM OBJETO GENERICO<T> ONDE ELE DEVE SER DO TIPO BaseEntity
+    ESSA INTERFACE UTILIZA TASKs PARA TRATAR A API POIS ELA PODE RECEBER METODOS SINCRONOS OU ASSINCRONOS
+    */
+    public interface IRespository<T> where T : BaseEntity
+    {
+        Task<T> InsertAsync(T item);
+        Task<T> UpdateAsync(T item);
+        Task<bool> DeleteAsync(Guid id);
+        Task<T> SelectAsync(Guid id);
+        Task<IEnumerable<T>> SelectAsync();
+
+    }
+}
