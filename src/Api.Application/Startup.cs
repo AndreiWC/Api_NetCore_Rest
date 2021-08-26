3bfc2770d8980738d4c8b11d32d8f9c21cf535c4
AndreiWC <37909054+AndreiWC@users.noreply.github.com>
2021-08-25 22:33:37 -0300

    Implementação do Swegger
    Implementação do Swegger para documentação da API

--- src/Api.Application/Startup.cs
+++ src/Api.Application/Startup.cs
@@ -10,6 +10,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.OpenApi.Models;
 
 namespace application
 {
@@ -28,6 +29,29 @@ namespace application
             ConfigureService.ConfigureDependenciesService(services);
             ConfigureRepository.ConfigureDependenciesRepository(services);
             services.AddControllers();
+            services.AddSwaggerGen(c =>{
+                c.SwaggerDoc("v1", new OpenApiInfo
+                {
+                    Version ="v1",
+                    Title="API com AspNetCore 3.1",
+                    Description="API Contruido para fins de conhecimento, a mesma foi baseada no modelo estrutural DDD",
+                    TermsOfService= new Uri("http://visualsoftware.inf.br"),
+                    Contact = new OpenApiContact{
+                        Name = "Andrei Welliton Corezolla",
+                        Email ="andrei@visualsoftware.inf.br",
+                        Url= new Uri("http://visualsoftware.inf.br"),
+                        
+                    },
+
+                    License = new OpenApiLicense{
+                        Name = "Termo de Licença de uso",                        
+                        Url= new Uri("http://visualsoftware.inf.br")
+                    }
+
+                    
+                });
+
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -37,6 +61,13 @@ namespace application
             {
                 app.UseDeveloperExceptionPage();
             }
+            
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.jason", "API com AspNetCore 3.1");
+                c.RoutePrefix = string.Empty;
+            });
 
             app.UseRouting();
 
